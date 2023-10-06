b35c37ca09374cc913218dec82a4a00182bae965
RPHA0004 <rpha0004@student.monash.edu>
2023-10-06 11:42:20 +1100

    WIP

---
+++ Final Demo/true_map.py
@@ -0,0 +1,20 @@
+import numpy as np
+import ast
+
+from SLAM_eval import parse_user_map
+
+
+aruco_markers = str('lab_output/slam.txt')
+fruits = open('lab_output/targets.txt', 'r')
+fruits = ast.literal_eval(fruits.read())
+aruco_markers = parse_user_map(aruco_markers)
+
+EstimateMap = {}
+for i in range(1,11):
+    EstimateMap[f"aruco{i}_0"] = {"y": float(aruco_markers[i][0]), "x": float(-aruco_markers[i][1])}
+
+EstimateMap.update(fruits)
+print(EstimateMap)
+
+with open('test.txt', 'w') as f:
+    write = f.write(str(EstimateMap))
